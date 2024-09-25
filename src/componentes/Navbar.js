055f0a09bc6011a5c71d6f9b0217e04878dacdf3
Vaibhav Mathur <129491099+VaibhavCoder123@users.noreply.github.com>
2024-09-25 16:29:16 +0530

    Update Navbar.js

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -6,9 +6,9 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="nav-link" to-current="page" to="/">
+          <h1>
             MonkeyMedia
-          </Link>
+          </h1>
           <button
             className="navbar-toggler"
             type="button"
